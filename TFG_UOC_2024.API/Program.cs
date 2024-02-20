3853ffb251e2b3e2f4b0a2f66a2e2b771a65d02f
Rubén Gómez Belchín <rgomezbe@uoc.edu>
2024-02-20 17:40:27 +0100

    program y startup

--- TFG_UOC_2024.API/Program.cs
+++ TFG_UOC_2024.API/Program.cs
@@ -1,6 +1,43 @@
-var builder = WebApplication.CreateBuilder(args);
-var app = builder.Build();
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
 
-app.MapGet("/", () => "Hello World!");
+namespace TFG_UOC_2024.API
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            // CreateHostBuilder(args).Build().Run();
+            CreateWebHostBuilder(args).Build().Run();
+        }
 
-app.Run();
+        /*
+        public static IHostBuilder CreateHostBuilder(string[] args) =>
+            Host.CreateDefaultBuilder(args)
+                .ConfigureWebHostDefaults(webBuilder =>
+                {
+                    webBuilder.UseStartup<Startup>();
+                });
+        */
+
+        public static IWebHostBuilder CreateWebHostBuilder(string[] args) =>
+           WebHost.CreateDefaultBuilder(args)
+               .ConfigureLogging((hostingContext, logging) =>
+               {
+                   logging.AddConfiguration(hostingContext.Configuration.GetSection("Logging"));
+               })
+                // For application running on Kestrel:
+                .ConfigureKestrel((context, options) =>
+                {
+                    options.Limits.MaxRequestBodySize = 737280000;
+                })
+               .UseStartup<Startup>();
+    }
+}
